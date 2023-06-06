143c54b575d28879af81699ab7fa9666f48519d3
OwenDes <owen.deschamps@etudiant.univ-rennes1.fr>
2023-06-06 16:26:55 +0200

    Delete local directory
    Signed-off-by: OwenDes <owen.deschamps@etudiant.univ-rennes1.fr>

--- local/js/dropdowns.js
+++
@@ -1,9 +0,0 @@
-var dropdowns = document.querySelectorAll(".dropdown")
-                dropdowns.forEach(function (dropdown) {
-                  dropdown.addEventListener("mouseenter", function () {
-                    dropdown.querySelector(".dropdown-menu").classList.add("show")
-                  })
-                  dropdown.addEventListener("mouseleave", function () {
-                    dropdown.querySelector(".dropdown-menu").classList.remove("show")
-                  })
-                })
