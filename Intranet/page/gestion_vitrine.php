f412637e7c9ee0819dfbc5c376ec309e86948b9f
BENEAT-Liam <liam.beneat@etudiant.univ-rennes1.fr>
2023-06-13 10:31:31 +0200

    correction fichier

--- Intranet/page/gestion_vitrine.php
+++ Intranet/page/gestion_vitrine.php
@@ -3,11 +3,11 @@
 <div class="container m-5">
     <div class="row justify-content-center align-items-center bg-light">
         <div class="col text-center">
-        <h1><a href="lecture_formualire.php"><i class="fa-sharp fa-solid fa-rectangle-list fa-2xl" style="color: #000000;"></i></a></h1>
+        <h1><a href="lecture_formulaire.php"><i class="fa-sharp fa-solid fa-rectangle-list fa-2xl" style="color: #000000;"></i></a></h1>
         <p>Lecture du formulaire de  la page Pompier volontaire</p>
         </div>
         <div class="col text-center">
-            <h1><a href="modif_formualaire.php"><i class="fa-solid fa-newspaper fa-2xl" style="color: #000000;"></i></a></h1>
+            <h1><a href="modif_formulaire.php"><i class="fa-solid fa-newspaper fa-2xl" style="color: #000000;"></i></a></h1>
             <p>Modification des actulaitées</p>
         </div>
     </div>
