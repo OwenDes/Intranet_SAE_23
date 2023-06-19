23cd9754e634300125e801ec1a897b96aa1dcf00
Garrix7512 <alexis.etudiant@univ-rennes1.fr>
2023-06-19 17:15:01 +0200

    correction bug

--- Intranet/page/partenaires.php
+++ Intranet/page/partenaires.php
@@ -1,44 +1,40 @@
 <?php include '../Fonction_Intranet.php'; header_Intranet() ?>
     
     
-<?php
+<div class="container text-center lead">
+        <h1 class="mb-4 pt-5">Partenariats</h1>
+    </div> 
+    <div class="container text-center lead">
+        <p><em>Sur cette page, nous souhaitons mettre en lumière nos partenaires et mécènes, véritables acteurs du progrès et de l'efficacité de notre mission. Leur soutien généreux et leur contribution active jouent un rôle essentiel dans la poursuite de nos objectifs communs.</em></p>
+    </div>
+<section>
+    
+    <div class="container offset-md-2 pt-5 pb-4">
+    
+    <?php
 
-$partnerData = json_decode(file_get_contents('../../Intranet/données/Partenaires.json'), true);
+$jsonFilePath = '../../Intranet/données/partenaires.json';
+$jsonData = file_get_contents($jsonFilePath);
+$data = json_decode($jsonData);
 
-if (!empty($partnerData)) {
-    echo '<div class="row">';
-    foreach ($partnerData as $partner) {
-        echo '<div class="col-md-6">';
-        echo '<img src="' . $partner['image'] . '" alt="' . $partner['description'] . '" class="img-thumbnail" style="width: 220px;">';
+if ($data !== null) {
+    foreach ($data as $item) {
+        $description = $item->description;
+        $image = $item->image;
+        $timestamp = time(); // Obtient le timestamp actuel
+
+        echo '<div class="container mb-5 lead">';
+        echo '<div class="container mb-5">';
+        echo '<img src="' . $image . '?t=' . $timestamp . '" alt="Image" class="img-fluid shadow rounded" width="170px">';
+        echo '</div>';
+        echo '<p>' . $description . '</p>';
         echo '</div>';
     }
-    echo '</div>';
-    echo '</div>';
+} else {
+    echo 'Erreur lors de la lecture du fichier JSON.';
 }
+
 ?>
-    </div>
-    <div class="container-fluid offset-md-2 col-sm-7 lead">
-        <p>
-        <?php
-// Lire le contenu du fichier JSON
-$jsonData = file_get_contents('../../Intranet/données/partenaires.json');
 
-// Décoder le contenu JSON en tableau associatif
-$data = json_decode($jsonData, true);
-
-// Parcourir les éléments du tableau
-foreach ($data as $item) {
-    // Récupérer la valeur de la variable "description"
-    $description = $item['description'];
-
-    echo $description;
-
-}
-?>
-        </p>
-    </div>
 </section>
-<div class="container mt-5 mb-5 text-center lead">
-        <p><strong><em>Nous sommes ravis d'annoncer l'arrivée prochaine d'un nouveau partenaire au sein du SNIS (Service National d'Incendie et de Secours) du Listemburg. Restez à l'écoute pour découvrir notre collaboration prometteuse !</em></strong></p>
-    </div>
 <?php pagefooter_Intranet() ;?>
