67c5cd9f2d832a36f654e9f44a17032922785e6e
Mathieu LE VEN <mathieu.le-ven@etudiant.univ-rennes1.fr>
2023-06-19 20:08:11 +0200

    Ajout de la lecture dans les modaux

--- Intranet/page/depot_fichier.php
+++ Intranet/page/depot_fichier.php
@@ -1,24 +1,20 @@
-<?php
-include '../Fonction_Intranet.php';
+<?php include '../Fonction_Intranet.php'; 
 session_start();
 if (!isset($_SESSION['user']) || !isset($_SESSION['role']) || ($_SESSION['role'] != 'user' && $_SESSION['role'] != 'admin')) {
     header('Location: ../page/connexion.php');
     exit();
 }
 
-header_Intranet();
-navbar_Intranet();
-?>
-
-<div class="container">
+header_Intranet(); navbar_Intranet() ?>
+  <div class="container">
     <h1>Upload de fichiers</h1>
 
     <form name='upload' method='post' action='' enctype='multipart/form-data'>
-        <div class="mb-3">
-            <label for="fichier" class="form-label">Choisir un fichier :</label>
-            <input type="file" class="form-control" id="fichier" name="fichier">
-        </div>
-        <button type="submit" name="valider" class="btn btn-primary">Envoyer</button>
+      <div class="mb-3">
+        <label for="fichier" class="form-label">Choisir un fichier :</label>
+        <input type="file" class="form-control" id="fichier" name="fichier">
+      </div>
+      <button type="submit" name="valider" class="btn btn-primary">Envoyer</button>
     </form>
 
     <?php
@@ -26,7 +22,7 @@ navbar_Intranet();
         $file = $_FILES['fichier'];
 
         // Vérifier le type de fichier
-        $allowed_extensions = array('pdf', 'txt', 'jpg', 'jpeg', 'png');
+        $allowed_extensions = array('pdf','txt', 'jpg', 'jpeg', 'png');
         $file_extension = pathinfo($file['name'], PATHINFO_EXTENSION);
 
         if (!in_array($file_extension, $allowed_extensions)) {
@@ -73,11 +69,11 @@ navbar_Intranet();
                 echo '<td>';
                 $file_path = $folder_path . '/' . $file;
                 $file_extension = pathinfo($file_path, PATHINFO_EXTENSION);
-
-                if (in_array($file_extension, array('pdf', 'txt', 'jpg', 'jpeg', 'png'))) {
+                
+                if (in_array($file_extension, array('pdf','txt', 'jpg', 'jpeg', 'png'))) {
                     echo '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#fileModal" data-file="' . $file_path . '">Voir</button>';
                 }
-
+                
                 echo '     <a href="' . $file_path . '" download class="btn btn-secondary">Télécharger</a>';
                 echo '</td>';
                 echo '</tr>';
@@ -93,59 +89,43 @@ navbar_Intranet();
 
     <!-- Modal -->
     <div class="modal fade" id="fileModal" tabindex="-1" aria-labelledby="fileModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="fileModalLabel">Contenu du fichier</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div id="fileViewer"></div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                </div>
-            </div>
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="fileModalLabel">Contenu du fichier</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p id="fileContent"></p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+          </div>
         </div>
+      </div>
     </div>
-</div>
 
-<script>
+  </div>
+  <script>
     var fileModal = document.getElementById('fileModal');
     fileModal.addEventListener('show.bs.modal', function (event) {
-        var button = event.relatedTarget;
-        var file = button.getAttribute('data-file');
+      var button = event.relatedTarget;
+      var file = button.getAttribute('data-file');
 
-        var fileViewer = document.getElementById('fileViewer');
-        fileViewer.innerHTML = ''; // Réinitialiser le contenu du fichier précédent
-
-        var fileExtension = file.split('.').pop().toLowerCase();
-        if (fileExtension === 'pdf') {
-            // Afficher un fichier PDF
-            fileViewer.innerHTML = '<iframe src="' + file + '" frameborder="0" width="100%" height="500px"></iframe>';
-        } else if (['jpg', 'jpeg', 'png'].includes(fileExtension)) {
-            // Afficher une image
-            fileViewer.innerHTML = '<img src="' + file + '" class="img-fluid" alt="Image">';
-        } else {
-            // Afficher le contenu du fichier texte
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        fileViewer.innerHTML = '<pre>' + xhr.responseText + '</pre>';
-                    } else {
-                        fileViewer.innerHTML = 'Erreur lors du chargement du fichier.';
-                    }
-                }
-            };
-            xhr.open('GET', file, true);
-            xhr.send();
+      // Charger le contenu du fichier via une requête AJAX
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            document.getElementById('fileContent').textContent = xhr.responseText;
+          } else {
+            document.getElementById('fileContent').textContent = 'Erreur lors du chargement du fichier.';
+          }
         }
+      };
+      xhr.open('GET', file, true);
+      xhr.send();
     });
-
-    fileModal.addEventListener('hidden.bs.modal', function () {
-        // Supprimer le contenu affiché lorsque le modal est fermé
-        var fileViewer = document.getElementById('fileViewer');
-        fileViewer.innerHTML = '';
-    });
-</script>
+  </script>
+</body>
+</html>
