5c2063a4721b544fdaecc3aa44f06210d3e45a5b
Garrix7512 <alexis.etudiant@univ-rennes1.fr>
2023-06-19 16:15:43 +0200

    correction bug

--- Frontal/page/partenaires.php
+++ Frontal/page/partenaires.php
@@ -59,26 +59,31 @@
     </div>
     <div class="container offset-md-2 pt-5 pb-4">
     
-<?php
+    <?php
 
 $jsonFilePath = '../../Intranet/données/partenaires.json';
 $jsonData = file_get_contents($jsonFilePath);
 $data = json_decode($jsonData);
 
-foreach ($data as $item) {
-    $description = $item->description;
-    $image = $item->image;
-    $timestamp = time(); // Obtient le timestamp actuel
-    
-    echo '<div class="container mb-5 lead">';
-    echo '<div class="container mb-5">';
-    echo '<img src="' . $image . '?t=' . $timestamp . '" alt="Image" class="img-fluid shadow rounded" width="170px">';
-    echo '</div>';
-    echo '<p>' . $description . '</p>';
-    echo '</div>';
+if ($data !== null) {
+    foreach ($data as $item) {
+        $description = $item->description;
+        $image = $item->image;
+        $timestamp = time(); // Obtient le timestamp actuel
+
+        echo '<div class="container mb-5 lead">';
+        echo '<div class="container mb-5">';
+        echo '<img src="' . $image . '?t=' . $timestamp . '" alt="Image" class="img-fluid shadow rounded" width="170px">';
+        echo '</div>';
+        echo '<p>' . $description . '</p>';
+        echo '</div>';
+    }
+} else {
+    echo 'Erreur lors de la lecture du fichier JSON.';
 }
 
 ?>
+
 </section>
 <div class="container mt-5 mb-5 text-center lead">
         <p><strong><em>Nous sommes ravis d'annoncer l'arrivée prochaine d'un nouveau partenaire au sein du SNIS (Service National d'Incendie et de Secours) du Listemburg. Restez à l'écoute pour découvrir notre collaboration prometteuse !</em></strong></p>
