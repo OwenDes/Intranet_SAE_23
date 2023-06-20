53eadab22b32b3fbb6c925adb18fad3f48228194
BENEAT-Liam <liam.beneat@etudiant.univ-rennes1.fr>
2023-06-20 08:07:51 +0200

    corrections modif formulaires

--- Intranet/traitement/modif_formulaire_traitement.php
+++ Intranet/traitement/modif_formulaire_traitement.php
@@ -2,7 +2,6 @@
 
 $titres = $_POST['titres'];
 $contenus = $_POST['contenus'];
-$liens = $_POST['liens'];
 $ids = $_POST['ids'];
 
 $json = file_get_contents('../données/actualites.json');
@@ -12,13 +11,12 @@ $actualites = json_decode($json, true);
 foreach ($ids as $index => $id) {
   $actualites[$id]['titre'] = $titres[$index];
   $actualites[$id]['contenu'] = $contenus[$index];
-  $actualites[$id]['lien'] = $liens[$index];
 }
 
 $json = json_encode($actualites, JSON_PRETTY_PRINT);
 file_put_contents("../données/actualites.json", $json);
 echo '<br>';
 
-header("Location: ../page/modif_formualaire.php");
+header("Location: ../page/modif_formulaire.php");
 exit;
 ?>
